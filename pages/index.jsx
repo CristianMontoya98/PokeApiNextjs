5ef1058b4913e7d5574e4197d2d7f80800ec99bf
Christian Montoya <chrimonlo@gmail.com>
2022-05-06 17:32:52 -0500

    Fetch modified, pokemon information aquired by each pokemon including all the necesary information

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,22 +4,37 @@ import { useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home(props) {
-  const { initialPokemonList } = props;
-  const [pokemonList, setPokemonList] = useState(initialPokemonList);
-  console.log(initialPokemonList);
+  const {pokemonsList } = props;
+  const [pokemonList, setPokemonList] = useState(pokemonsList);
+  console.log(pokemonsList);
+  
   return (
     <>
+      
       <h1>PokeApi</h1>
+      
+      {pokemonList.map((pokemon, index) => <h2 key={index}>{pokemon.name}</h2> )}
     </>
   )
 }
 
-export async function getServerSideProps() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
-  const initialPokemonList = await response.json();
+export async function getStaticProps() {
+  let pokemonsList = [];
+  let list = [];
+  let data;
+  for (let i = 1; i <= 5; i++) { 
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    data = await res.json();
+    pokemonsList.push({
+      id: data.id,
+      name: data.name,
+      img: data.sprites.front_default,
+      type: data.types[0].type.name
+    })  
+  }
   return {
     props: {
-      initialPokemonList
+      pokemonsList
     }
   }
 }
